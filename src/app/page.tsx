735bf36167319432fca231fdd758a08bb85bedc1
Alison G. Martins <odevalison@gmail.com>
2025-08-30 22:33:30 -0300

    feat: add home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function HomePage() {
   return (
-    <div>
-      <h1>Hello from Home Page!</h1>
-    </div>
+    <main className="flex w-7xl max-w-full flex-col items-center justify-center">
+      <h2 className="text-xl/[150%] font-medium md:text-2xl">
+        Manage your company
+      </h2>
+      <h1 className="text-3xl/[150%] font-bold text-blue-800 md:text-4xl">
+        Services, Customers
+      </h1>
+
+      <Image
+        src="/assets/hero-image.svg"
+        alt="Dev Control from hero image"
+        width={600}
+        height={400}
+        className="mt-11 max-w-xs md:max-w-xl"
+      />
+    </main>
   );
 }
--- src/components/container/index.tsx
+++ src/components/container/index.tsx
@@ -1,5 +1,7 @@
 export function Container({ children }: { children: React.ReactNode }) {
   return (
-    <div className="h-[calc(100dvh-80px)] w-dvw px-5 py-10">{children}</div>
+    <div className="flex min-h-[calc(100dvh-80px)] w-dvw items-center justify-center px-5 py-10">
+      {children}
+    </div>
   );
 }
