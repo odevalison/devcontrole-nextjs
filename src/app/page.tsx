3cd73862fc4996866b94b8d03bd18d8d5780e4f2
Alison G. Martins <odevalison@gmail.com>
2025-08-31 09:58:08 -0300

    feat: ensure hero image and hero title has ideal size mobile and desktop

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,19 +6,19 @@ export default function HomePage() {
   return (
     <Container>
       <main className="flex min-h-[calc(100dvh-120px)] flex-col items-center justify-center">
-        <h2 className="text-xl/[150%] font-medium md:text-2xl/[150%]">
+        <h2 className="text-lg/[150%] font-medium md:text-2xl/[150%]">
           Gerencie sua empresa
         </h2>
-        <h1 className="text-3xl/[150%] font-bold text-blue-800 md:text-4xl/[150%]">
+        <h1 className="text-2xl/[150%] font-bold text-blue-800 md:text-4xl/[150%]">
           Atendimentos, Clientes
         </h1>
 
         <Image
           src="/assets/hero-image.svg"
           alt="Dev Control from hero image"
-          width={600}
-          height={400}
-          className="mt-11 max-w-xs md:max-w-xl"
+          width={500}
+          height={300}
+          className="mt-8 max-w-72 md:max-w-lg"
         />
       </main>
     </Container>
