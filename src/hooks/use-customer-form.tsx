e05fd591145c33e66dc4c99504e2a37bc770b631
Alison G. Martins <odevalison@gmail.com>
2025-09-05 15:45:15 -0300

    refactor: add useCustomerForm hook

---
+++ src/hooks/use-customer-form.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import {
+  newCustomerFormSchema,
+  NewCustomerFormSchemaData,
+} from "@/app/dashboard/customers/components/form/schema";
+
+export function useCustomerForm() {
+  return useForm<NewCustomerFormSchemaData>({
+    resolver: zodResolver(newCustomerFormSchema),
+    mode: "onSubmit",
+    defaultValues: { email: "", fullName: "", phone: "", address: "" },
+  });
+}
