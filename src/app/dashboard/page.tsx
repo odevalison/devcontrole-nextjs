bef85ddea9e223d2a32a00ee001cef9699e225bc
Alison G. Martins <odevalison@gmail.com>
2025-09-05 03:28:17 -0300

    refactor: move Container component and getServerSession to dashboard layout

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,21 +1,7 @@
-import { redirect } from "next/navigation";
-import { getServerSession } from "next-auth";
-
-import { Container } from "@/components/container";
-import { authOptions } from "@/lib/auth";
-
 export default async function DashboardPage() {
-  const session = await getServerSession(authOptions);
-
-  if (!session || !session?.user) {
-    redirect("/");
-  }
-
   return (
-    <Container>
-      <main>
-        <h1>Dashboard page</h1>
-      </main>
-    </Container>
+    <main>
+      <h1>Dashboard page</h1>
+    </main>
   );
 }
