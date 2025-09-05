140a91aaa3c816fb0633577eb122a08bbd5f56bd
Alison G. Martins <odevalison@gmail.com>
2025-09-05 02:33:52 -0300

    feat: add handle login and logout functions on Header component

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -1,7 +1,34 @@
-import { LogOut, UserCircle2 } from "lucide-react";
+"use client";
+
+import { Loader2, Lock, LogOut, UserCircle2 } from "lucide-react";
 import Link from "next/link";
+import { signIn, signOut, useSession } from "next-auth/react";
 
 export function Header() {
+  const { status } = useSession();
+
+  const handleLogInUser = async () => {
+    try {
+      await signIn();
+    } catch (error) {
+      if (error instanceof Error) {
+        throw Error(error.message);
+      }
+      throw new Error("Não foi possível fazer login.");
+    }
+  };
+
+  const handleLogOutUser = async () => {
+    try {
+      await signOut();
+    } catch (error) {
+      if (error instanceof Error) {
+        throw Error(error.message);
+      }
+      throw new Error("Não foi possível sair da conta");
+    }
+  };
+
   return (
     <header className="flex h-20 w-full bg-white px-5 shadow-sm">
       <nav className="mx-auto flex w-full max-w-7xl items-center justify-between">
@@ -11,14 +38,28 @@ export function Header() {
           </h1>
         </Link>
 
-        <div className="flex items-baseline gap-3">
-          <Link href="/dashboard" className="text-zinc-500">
-            <UserCircle2 className="size-7" />
-          </Link>
-          <button className="text-zinc-500">
-            <LogOut className="size-7" />
+        {status === "loading" && (
+          <button>
+            <Loader2 className="size-7 animate-spin text-zinc-500" />
           </button>
-        </div>
+        )}
+
+        {status === "unauthenticated" && (
+          <button onClick={handleLogInUser}>
+            <Lock className="size-7 text-zinc-500" />
+          </button>
+        )}
+
+        {status === "authenticated" && (
+          <div className="flex items-baseline gap-3">
+            <Link href="/dashboard" className="text-zinc-500">
+              <UserCircle2 className="size-7" />
+            </Link>
+            <button className="text-zinc-500" onClick={handleLogOutUser}>
+              <LogOut className="size-7" />
+            </button>
+          </div>
+        )}
       </nav>
     </header>
   );
