71a48bba09c3b31f41a3f30ddfe1532a94dc3e4a
Alison G. Martins <odevalison@gmail.com>
2025-09-05 00:35:47 -0300

    chore: add husky and lint-staged setup

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,5 +1,5 @@
 import { FlatCompat } from "@eslint/eslintrc";
-import simpleImportSort from 'eslint-plugin-simple-import-sort'
+import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 
@@ -14,11 +14,12 @@ const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
   {
     plugins: {
-      "simple-import-sort": simpleImportSort
+      "simple-import-sort": simpleImportSort,
     },
     rules: {
       "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error"
+      "simple-import-sort/exports": "error",
+      "no-console": "error",
     },
     ignores: [
       "node_modules/**",
