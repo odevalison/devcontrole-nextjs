bb9cce5e3b0e5352623fefa749c2c1a174a40405
Alison G. Martins <odevalison@gmail.com>
2025-09-05 15:47:30 -0300

    feat: add smooth open/close modal transition on NewCustomerModal component

--- src/app/dashboard/customers/components/new-customer-modal/index.tsx
+++ src/app/dashboard/customers/components/new-customer-modal/index.tsx
@@ -1,18 +1,42 @@
 "use client";
 
+import "./styles.css";
+
+import { useRef } from "react";
 import { createPortal } from "react-dom";
+import { CSSTransition } from "react-transition-group";
 
 import { useModal } from "@/context/modal";
 
+import { NewCustomerForm } from "../form";
+
 export function NewCustomerModal() {
   const { modalIsOpen } = useModal();
+  const modalRef = useRef<HTMLDivElement>(null);
 
-  if (!modalIsOpen) return null;
+  return (
+    <CSSTransition
+      in={modalIsOpen}
+      timeout={500}
+      nodeRef={modalRef}
+      classNames="new-customer-modal"
+      unmountOnExit
+    >
+      <>
+        {createPortal(
+          <div
+            ref={modalRef}
+            className="size-screen fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black/10 p-5 backdrop-blur"
+          >
+            <div className="flex w-md max-w-full flex-col items-center justify-center space-y-5 rounded-xl bg-white p-5 shadow">
+              <h1 className="text-2xl font-bold">Novo cliente</h1>
 
-  return createPortal(
-    <div className="size-screen fixed top-0 right-0 bottom-0 left-0 flex items-center justify-center bg-black/20 backdrop-blur-sm">
-      <div className="size-[400px] rounded-xl bg-white shadow"></div>
-    </div>,
-    document.body,
+              <NewCustomerForm />
+            </div>
+          </div>,
+          document.body,
+        )}
+      </>
+    </CSSTransition>
   );
 }
