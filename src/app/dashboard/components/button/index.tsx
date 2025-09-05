f4940174771a270c4366ba4181dec8441ff6fb14
Alison G. Martins <odevalison@gmail.com>
2025-09-05 04:27:46 -0300

    feat: add Button component

---
+++ src/app/dashboard/components/button/index.tsx
@@ -0,0 +1,48 @@
+import { HTMLAttributes } from "react";
+
+type ButtonVariants = "primary" | "secondary" | "danger";
+type ButtonSizes = "sm" | "lg" | "icon";
+
+interface ButtonProps extends HTMLAttributes<HTMLButtonElement> {
+  children: React.ReactNode;
+  variant?: ButtonVariants;
+  size?: ButtonSizes;
+}
+
+export function Button({
+  children,
+  size = "sm",
+  variant = "primary",
+  ...props
+}: ButtonProps) {
+  const getSizeClasses = () => {
+    switch (size) {
+      case "sm":
+        return "px-4 py-1 text-sm";
+      case "lg":
+        return "px-6 py-2";
+      case "icon":
+        return "p-1.5 size-fit *:size-5";
+    }
+  };
+
+  const getVariantClasses = () => {
+    switch (variant) {
+      case "primary":
+        return "bg-blue-500 hover:bg-blue-500/75";
+      case "danger":
+        return "bg-rose-500 hover:bg-rose-500/75";
+      case "secondary":
+        return "bg-zinc-500 hover:bg-zinc-500/75";
+    }
+  };
+
+  return (
+    <button
+      {...props}
+      className={`rounded-md font-semibold text-white drop-shadow transition ${getSizeClasses()} ${getVariantClasses()}`}
+    >
+      {children}
+    </button>
+  );
+}
