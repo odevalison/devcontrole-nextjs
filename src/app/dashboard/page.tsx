9e060f0c1b0ae884e0f4679d8a1e90c98373b025
Alison G. Martins <odevalison@gmail.com>
2025-09-18 22:56:18 -0300

    feat: ensure only authenticated users can access dashboard page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,17 @@
 import Link from 'next/link'
+import { redirect } from 'next/navigation'
+import { getServerSession } from 'next-auth'
+
+import { authOptions } from '@/lib/auth'
 
 import { Button } from '../../components/button'
 import { TicketsTable } from './components/tickets-table'
 
 const DashboardPage = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session || !session.user) {
+    redirect('/')
+  }
   return (
     <main className="space-y-6.5">
       <div className="flex items-center justify-between">
