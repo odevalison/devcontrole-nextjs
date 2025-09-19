c683348e1b99e2190d60eba6bd67b945ae19a85d
Alison G. Martins <odevalison@gmail.com>
2025-09-18 22:29:59 -0300

    feat: add getUserCustomers action

---
+++ src/app/actions/get-user-customers/index.ts
@@ -0,0 +1,22 @@
+'use server'
+
+import { getServerSession } from 'next-auth'
+
+import { authOptions } from '@/lib/auth'
+import prismaClient from '@/lib/prisma'
+import { Customer } from '@/utils/customer.type'
+
+export const getUserCustomers = async () => {
+  const session = await getServerSession(authOptions)
+  if (!session || !session?.user) {
+    throw new Error('Usuário não autenticado')
+  }
+  const userCustomers = await prismaClient.customer.findMany({
+    where: { userId: session.user.id },
+  })
+  const userDontHaveCustomers = !userCustomers.length
+  if (userDontHaveCustomers) {
+    return null
+  }
+  return userCustomers as Customer[]
+}
